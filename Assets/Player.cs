641de913b9db12931d33fa7074442ac19682de2f
3-jn-3 <mikasakata333@gmail.com>
2026-04-13 14:48:32 +0800

    yup

---
+++ Assets/Player.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5f;
+
+    Rigidbody2D rb;
+
+    private Vector2 movement;
+    private Animator animator;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    public void OnMove(InputValue value)
+    {
+        movement = value.Get<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (rb == null)
+            return;
+
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    private void Update()
+    {
+        if (animator == null)
+            return;
+
+        if (movement != Vector2.zero)
+        {
+            animator.SetFloat("X Pos", movement.x);
+            animator.SetFloat("Y Pos", movement.y);
+        }
+
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+}
